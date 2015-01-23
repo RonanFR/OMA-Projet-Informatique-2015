cb467b9c42c2a39bb86e909175d8f6a543711c32
Hamza GHRIB <hamza.ghrib@gmail.com>
2015-01-23 23:07:40 +0100

    Ava,cement projet Question 18
    Signed-off-by: Hamza GHRIB <hamza.ghrib@gmail.com>

--- Projet Informatique/TestSudoku/main.cpp
+++ Projet Informatique/TestSudoku/main.cpp
@@ -1,4 +1,5 @@
 # include "Cell.h"
+#include "Grid.h"
 #include<iostream>
 # include <stdexcept>
 
@@ -8,17 +9,30 @@ using namespace std;
 
 int main(){
 
-	
-	try{
-		Cell cell1(10);
-	}
-	catch (invalid_argument& ia)
-	{
+
+	/*try{
+		Cell cell1(9);
+		}
+		catch (invalid_argument& ia)
+		{
 		cerr << ia.what() << endl;
+		}
+		*/
+	list<Region> regions1;
+	Region region1("482-9--85-");
+
+	for (int i = 1; i < 10; i++)
+	{
+		regions1.push_back(region1);
+
 	}
-
-
-
+	try {
+		Grid Grid(regions1);
+	}
+	catch (length_error & il)
+	{
+		cerr << il.what() << endl;
+	}
 	getchar();
 	return 0;
 }
