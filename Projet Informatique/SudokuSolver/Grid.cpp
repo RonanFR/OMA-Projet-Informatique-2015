79d0a2fbb5e0d4729dcb534eb641c70ab93c0543
Ronan Fruit <ronan@fruit.nom.fr>
2015-01-31 00:50:33 +0100

    Test Grid : visiteurs simples colonnes et lignes
    Signed-off-by: Ronan Fruit <ronan@fruit.nom.fr>

--- Projet Informatique/SudokuSolver/Grid.cpp
+++ Projet Informatique/SudokuSolver/Grid.cpp
@@ -90,60 +90,60 @@ RegionHolder  Grid::getSO()
 
 void Grid::dispGrid()
 {
-	cout << "  -----------------------------" << endl;
+	cout << "  ------------------------------------" << endl;
 
 	// 1st row
 	cout << " | " << NO.getNO() << " | " << NO.getN() << " | " << NO.getNE() << " | ";
-	cout << "| " << N.getNO() << " | " << N.getN() << " | " << N.getNE() << " | ";
-	cout << "| " << NE.getNO() << " | " << NE.getN() << " | " << NE.getNE() << " | ";
-	cout << "  -----------------------------" << endl;
+	cout  << N.getNO() << " | " << N.getN() << " | " << N.getNE() << " | ";
+	cout  << NE.getNO() << " | " << NE.getN() << " | " << NE.getNE() << " | " << endl;
+	cout << "  ------------------------------------" << endl;
 
 	// 2nd row
 	cout << " | " << NO.getO() << " | " << NO.getC() << " | " << NO.getE() << " | ";
-	cout << "| " << N.getO() << " | " << N.getC() << " | " << N.getE() << " | ";
-	cout << "| " << NE.getO() << " | " << NE.getC() << " | " << NE.getE() << " | ";
-	cout << "  -----------------------------" << endl;
+	cout  << N.getO() << " | " << N.getC() << " | " << N.getE() << " | ";
+	cout  << NE.getO() << " | " << NE.getC() << " | " << NE.getE() << " | " << endl;
+	cout << "  ------------------------------------" << endl;
 
 	// 3rd row
 	cout << " | " << NO.getSO() << " | " << NO.getS() << " | " << NO.getSE() << " | ";
-	cout << "| " << N.getSO() << " | " << N.getS() << " | " << N.getSE() << " | ";
-	cout << "| " << NE.getSO() << " | " << NE.getS() << " | " << NE.getSE() << " | ";
-	cout << "  -----------------------------" << endl;
+	cout  << N.getSO() << " | " << N.getS() << " | " << N.getSE() << " | ";
+	cout  << NE.getSO() << " | " << NE.getS() << " | " << NE.getSE() << " | " << endl;
+	cout << "  ------------------------------------" << endl;
 
 	// 4th row
 	cout << " | " << O.getNO() << " | " << O.getN() << " | " << O.getNE() << " | ";
-	cout << "| " << C.getNO() << " | " << C.getN() << " | " << C.getNE() << " | ";
-	cout << "| " << E.getNO() << " | " << E.getN() << " | " << E.getNE() << " | ";
-	cout << "  -----------------------------" << endl;
+	cout  << C.getNO() << " | " << C.getN() << " | " << C.getNE() << " | ";
+	cout  << E.getNO() << " | " << E.getN() << " | " << E.getNE() << " | " << endl;
+	cout << "  ------------------------------------" << endl;
 
 	// 5th row
 	cout << " | " << O.getO() << " | " << O.getC() << " | " << O.getE() << " | ";
-	cout << "| " << C.getO() << " | " << C.getC() << " | " << C.getE() << " | ";
-	cout << "| " << E.getO() << " | " << E.getC() << " | " << E.getE() << " | ";
-	cout << "  -----------------------------" << endl;
+	cout  << C.getO() << " | " << C.getC() << " | " << C.getE() << " | ";
+	cout  << E.getO() << " | " << E.getC() << " | " << E.getE() << " | " << endl;
+	cout << "  ------------------------------------" << endl;
 
 	// 6th row
 	cout << " | " << O.getSO() << " | " << O.getS() << " | " << O.getSE() << " | ";
-	cout << "| " << C.getSO() << " | " << C.getS() << " | " << C.getSE() << " | ";
-	cout << "| " << E.getSO() << " | " << E.getS() << " | " << E.getSE() << " | ";
-	cout << "  -----------------------------" << endl;
+	cout  << C.getSO() << " | " << C.getS() << " | " << C.getSE() << " | ";
+	cout  << E.getSO() << " | " << E.getS() << " | " << E.getSE() << " | " << endl;
+	cout << "  ------------------------------------" << endl;
 
 	// 7th row
 	cout << " | " << SO.getNO() << " | " << SO.getN() << " | " << SO.getNE() << " | ";
-	cout << "| " << S.getNO() << " | " << S.getN() << " | " << S.getNE() << " | ";
-	cout << "| " << SE.getNO() << " | " << SE.getN() << " | " << SE.getNE() << " | ";
-	cout << "  -----------------------------" << endl;
+	cout  << S.getNO() << " | " << S.getN() << " | " << S.getNE() << " | ";
+	cout  << SE.getNO() << " | " << SE.getN() << " | " << SE.getNE() << " | " << endl;
+	cout << "  ------------------------------------" << endl;
 
 	// 8th row
 	cout << " | " << SO.getO() << " | " << SO.getC() << " | " << SO.getE() << " | ";
-	cout << "| " << S.getO() << " | " << S.getC() << " | " << S.getE() << " | ";
-	cout << "| " << SE.getO() << " | " << SE.getC() << " | " << SE.getE() << " | ";
-	cout << "  -----------------------------" << endl;
+	cout << S.getO() << " | " << S.getC() << " | " << S.getE() << " | ";
+	cout  << SE.getO() << " | " << SE.getC() << " | " << SE.getE() << " | " << endl;
+	cout << "  ------------------------------------" << endl;
 
 	// 9th row
 	cout << " | " << SO.getSO() << " | " << SO.getS() << " | " << SO.getSE() << " | ";
-	cout << "| " << S.getSO() << " | " << S.getS() << " | " << S.getSE() << " | ";
-	cout << "| " << SE.getSO() << " | " << SE.getS() << " | " << SE.getSE() << " | ";
-	cout << "  -----------------------------" << endl;
+	cout << S.getSO() << " | " << S.getS() << " | " << S.getSE() << " | ";
+	cout << SE.getSO() << " | " << SE.getS() << " | " << SE.getSE() << " | " << endl;
+	cout << "  ------------------------------------" << endl;
 	
 }
--- Projet Informatique/TestSudoku/main.cpp
+++ Projet Informatique/TestSudoku/main.cpp
@@ -2,6 +2,8 @@
 # include "Grid.h"
 # include <iostream>
 # include <stdexcept>
+# include "OnlyOneChoiceInRowVisitor.h"
+# include "OnlyOneChoiceInColumnVisitor.h"
 
 
 using namespace std;
@@ -9,33 +11,70 @@ using namespace std;
 
 int main(){
 
-	// Test 1
-	try{
-		Cell cell1(9);
-	}
-	catch (invalid_argument& ia)
-	{
-		cerr << ia.what() << endl;
-	}
+	//// Test 1
+	//try{
+	//	Cell cell1(9);
+	//}
+	//catch (invalid_argument& ia)
+	//{
+	//	cerr << ia.what() << endl;
+	//}
 
-	// Test 2
-	list<Region> regions1;
-	Region region1("482-9--85-");
+	//// Test 2
+	//list<Region> regions1;
+	//Region region1("482-9--85-");
 
-	for (int i = 1; i < 10; i++)
-	{
-		regions1.push_back(region1);
+	//for (int i = 1; i < 10; i++)
+	//{
+	//	regions1.push_back(region1);
 
-	}
+	//}
 
-	// Test 3
-	try {
+	//// Test 3
+	/*try {
 		Grid Grid(regions1);
 	}
 	catch (length_error & il)
 	{
 		cerr << il.what() << endl;
-	}
+	}*/
+
+	Region region1("482------");
+	Region region2("-56-9----");
+	Region region3("793------");
+	Region region4("---------");
+	Region region5("-8--6--2-");
+	Region region6("---------");
+	Region region7("------78-");
+	Region region8("-4--7-231");
+	Region region9("------456");
+	
+	list<Region> regions;
+	regions.push_back(region1);
+	regions.push_back(region2);
+	regions.push_back(region3);
+	regions.push_back(region4);
+	regions.push_back(region5);
+	regions.push_back(region6);
+	regions.push_back(region7);
+	regions.push_back(region8);
+	regions.push_back(region9);
+
+	Grid grid1(regions);
+	grid1.dispGrid();
+
+	OnlyOneChoiceInRowVisitor visitor1;
+	visitor1.Visit(grid1);
+
+	cout << endl << endl;
+	grid1.dispGrid();
+
+	OnlyOneChoiceInColumnVisitor visitor2;
+	visitor2.Visit(grid1);
+	
+	cout << endl << endl;
+	grid1.dispGrid();
+
 	getchar();
 	return 0;
 }
